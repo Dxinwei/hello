6af6e27a4d5dbbb8101a269a07c2ca651e40bd83
Dxinwei <107739687+Dxinwei@users.noreply.github.com>
2022-10-27 15:52:41 +0800

    test

---
+++ test.py
@@ -0,0 +1,26 @@
+import torch
+import cv2
+
+# def cv_show(name,img):
+#     cv2.imshow(name, img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+#
+# img = cv2.imread('D:/pythonProject/dataset/otherdatatset/me.jpg')
+# print(img.shape)
+# img2 = cv2.resize(img, (288, 288))
+# print(img2.shape)
+# #cv_show('test', img2)
+# # cv2.imwrite('D:/pythonProject/dataset/otherdatatset/test2.jpg', img2)
+# img3 = cv2.resize(img2, (1024, 1024))
+#
+# img4 = cv2.resize(img3, (288, 288))
+#
+# img5 = cv2.resize(img4, (1024, 1024))
+#
+# img6 = cv2.resize(img5, (288, 288))
+#
+# cv_show('test', img6)
+# cv2.imwrite('D:/pythonProject/dataset/otherdatatset/me1.jpg', img6)
+
+print(torch.__version__)
